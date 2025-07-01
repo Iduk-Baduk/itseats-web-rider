1bc67e47f07551192ff086cee8ee45dbd00933c7
한상희 <tkdgml822@gmail.com>
2025-07-01 11:59:26 +0900

    chore: 리덕스 설정

--- src/Store.js
+++ src/Store.js
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+// 임시 더미 reducer (나중에 실제 reducer로 교체) -> 없으면 오류 발생
+const dummyReducer = (state = {}, action) => {
+  return state;
+};
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    dummy: dummyReducer,
+  },
   devTools: true,
 });
 
