413d654cc90f3f98e339f11af37137626953d988
한상희 <tkdgml822@gmail.com>
2025-07-01 12:00:03 +0900

    feat: 사용자 위치 가져오기

--- src/pages/delivery/Map.jsx
+++ src/pages/delivery/Map.jsx
@@ -1,7 +1,28 @@
 import { Link } from "react-router-dom";
 import CommonMap from "../../components/common/CommonMap";
+import useLocation from "../../hooks/useLocation";
 
 export default function Map() {
+  // 현재 위치를 가져오는 커스텀 훅
+  const location = useLocation();
+
+  if (!location) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <p>위치 정보를 불러오는 중입니다...</p>
+      </div>
+    );
+  }
+
+  
+
   return (
     <div
       style={{
@@ -15,9 +36,9 @@ export default function Map() {
     >
       <h1>메인 페이지 (지도 및 배달)</h1>
       <CommonMap
-        lat={37.5665}
-        lng={126.978}
-        markers={[{ lat: 37.5665, lng: 126.978, type: "delivery" }]}
+        lat={location?.latitude}
+        lng={location?.longitude}
+        markers={[{ lat: location?.latitude, lng: location?.longitude, type: "delivery" }]}
         height="100vh"
         level={3}
       />
