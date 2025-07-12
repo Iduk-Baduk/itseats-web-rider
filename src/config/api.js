16184127de065681d9c0dc35589893235db5d125
한상희 <tkdgml822@gmail.com>
2025-07-13 05:42:29 +0900

    feat: endPoint 추가

--- src/config/api.js
+++ src/config/api.js
@@ -5,5 +5,12 @@ export const API_CONFIG = {
 export const API_ENDPOINTS = {
   // 배달 관련
   READY_ORDER: () => "api/rider/request",
-  // 필요에 따라 추가 엔드포인트 정의
+  ORDER_DETAILS: (orderId) => `api/rider/${orderId}/details`,
+  ACCEPT_ORDER: (orderId) => `api/rider/${orderId}/accept`,
+  REJECT_ORDER: (orderId) => `api/rider/${orderId}/reject`,
+  ARRIVED_STORE: (orderId) => `api/rider/${orderId}/arrived`,
+  PICKUP_ORDER: (orderId) => `api/rider/${orderId}/pickup`,
+  COMPLETE_ORDER: (orderId) => `api/rider/${orderId}/done`,
+  UPLOAD_PHOTO: (orderId) => `api/rider/${orderId}/picture`,
+  WORKING_STATUS: () => "api/rider/working",
 };
